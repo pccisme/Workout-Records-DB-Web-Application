8b9b6e883f925467ad2951a63151c3d442c0bcd4
pccisme <pccisme@gmail.com>
2020-07-09 02:57:16 -0500

    bootstrap

--- app.py
+++ app.py
@@ -10,20 +10,22 @@ db = SQLAlchemy(app)
 
 class Record(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    day = db.Column(db.String(50), nullable=False)
+    category = db.Column(db.String(50), nullable=False)
     exercise = db.Column(db.String(40), nullable=False)
     weight = db.Column(db.String(10), nullable=False)
     set = db.Column(db.String(10), nullable=False)
+    rep = db.Column(db.String(10), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.now)
 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.form:
-        record = Record(day=request.form.get("day"),
+        record = Record(category=request.form.get("category"),
                         exercise=request.form.get("exercise"),
                         weight=request.form.get("weight"),
-                        set=request.form.get("set"))
+                        set=request.form.get("sets"),
+                        rep=request.form.get("reps"))
         db.session.add(record)
         db.session.commit()
 
